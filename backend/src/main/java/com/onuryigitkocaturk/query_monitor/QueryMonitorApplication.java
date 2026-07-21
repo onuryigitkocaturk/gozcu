72ef87642016471b85d747c55fa94add3441d729
Onur Yiğit Kocatürk <onuryigitkocaturk@gmail.com>
2026-07-21 10:02:02 +0300

    first commit

---
+++ backend/src/main/java/com/onuryigitkocaturk/query_monitor/QueryMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.onuryigitkocaturk.query_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryMonitorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/onuryigitkocaturk/query_monitor/QueryMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onuryigitkocaturk.query_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
