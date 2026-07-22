4301aa894909471db48078aa585cd91dfbd9e660
Onur Yiğit Kocatürk <onuryigitkocaturk@gmail.com>
2026-07-22 10:59:06 +0300

    Add RegisterRequest DTO and DuplicateUserException

---
+++ backend/src/main/java/com/onuryigitkocaturk/query_monitor/exception/DuplicateUserException.java
@@ -0,0 +1,8 @@
+package com.onuryigitkocaturk.query_monitor.exception;
+
+public class DuplicateUserException extends RuntimeException {
+
+    public DuplicateUserException(String message) {
+        super(message);
+    }
+}
